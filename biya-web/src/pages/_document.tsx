25e8c58472c546021d226484bbf3511d9f99d7a0
biubiubiupiu21 <94020092+timyao21@users.noreply.github.com>
2024-09-12 13:42:50 -0400

    next.js set up

---
+++ biya-web/src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <meta charSet="utf-8" />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Symbols+Outlined"
+        rel="stylesheet"
+      />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ biya-web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return ( 
+    <div>
+      <h2>BIYA</h2>
+    </div>
+  );
+}
