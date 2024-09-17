df62395e8a155228984f4f0e73ff25f6dedcb411
biubiubiupiu21 <94020092+timyao21@users.noreply.github.com>
2024-09-17 11:38:40 -0400

    borad and sec test

--- biya-web/src/components/footer.tsx
+++ biya-web/src/components/footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 export default function Footer() {
   return (
@@ -7,37 +8,49 @@ export default function Footer() {
       <footer className=" bg-biya-gray py-8">
         <div className="container mx-auto flex flex-col items-center justify-between space-y-6 md:flex-row md:space-y-0">
           {/* Logo / Brand Name */}
-          <div className="text-center md:text-left">
-            <h2 className="text-2xl font-bold">
-              Baiya International Group Inc.
-            </h2>
-          </div>
-          <div className="flex space-x-4">
-            <Link href="/" passHref>
-              <Typography className="hover:text-biya-blue">Home</Typography>
-            </Link>
-            <Link href="/about" passHref>
-              <Typography className="hover:text-biya-blue">About Us</Typography>
-            </Link>
-            <Link href="/investors-overview" passHref>
-              <Typography className="hover:text-biya-blue">
-                Investors
-              </Typography>
-            </Link>
-            <Link href="/contact-us" passHref>
-              <Typography className="hover:text-biya-blue">Contact</Typography>
-            </Link>
-          </div>
 
-          <div className="text-center md:text-left">
-            <Typography className="font-bold">Contact Us:</Typography>
-            <Typography>Suite 7540</Typography>
-            <Typography>The Empire State Building, 350 Fifth Ave</Typography>
-            <Typography>New York, NY, 10118 USA</Typography>
-          </div>
+          <Stack
+            spacing={{ xs: 1, sm: 2 }}
+            direction={{ sm: "column", md: "row" }}
+            useFlexGap
+            sx={{ flexWrap: "wrap" }}
+            className="items-center"
+          >
+            <div className="text-center md:text-left">
+              <h2 className="text-2xl font-bold">
+                Baiya International Group Inc.
+              </h2>
+            </div>
+            <div className="flex space-x-4">
+              <Link href="/" passHref>
+                <Typography className="hover:text-biya-blue">Home</Typography>
+              </Link>
+              <Link href="/about" passHref>
+                <Typography className="hover:text-biya-blue">
+                  About Us
+                </Typography>
+              </Link>
+              <Link href="/investors-overview" passHref>
+                <Typography className="hover:text-biya-blue">
+                  Investors
+                </Typography>
+              </Link>
+              <Link href="/contact-us" passHref>
+                <Typography className="hover:text-biya-blue">
+                  Contact
+                </Typography>
+              </Link>
+            </div>
+
+            <div className="text-left px-3">
+              <Typography className="font-bold">Contact Us:</Typography>
+              <Typography>Suite 7540</Typography>
+              <Typography>The Empire State Building, 350 Fifth Ave</Typography>
+              <Typography>New York, NY, 10118 USA</Typography>
+            </div>
+          </Stack>
         </div>
 
-        {/* Copyright Notice */}
         <div className="mt-8 text-center text-sm text-gray-400">
           &copy; {new Date().getFullYear()} BIYA. All rights reserved.
         </div>
--- biya-web/src/pages/investors/corporate.tsx
+++ biya-web/src/pages/investors/corporate.tsx
@@ -1,11 +1,26 @@
 import IrPageBanner from "../../components/banner/irPageBanner";
+import Board from "../../components/investors/Board";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
+import Image from "next/image";
 
 export default function Corporate() {
   return (
     <div className="container pt-[60px]">
       <IrPageBanner title="Corporate" />
-      <div>
-
+      <Typography>Board of Directors</Typography>
+      <div className="py-5">
+        <Board />
+      </div>
+      <Divider />
+      <div className="py-3">
+        <Image
+          src="https://res.cloudinary.com/aehl/image/upload/v1726250854/table_r5jtij.png"
+          width={700}
+          height={500}
+          alt="Table of Board of Directors"
+        />
       </div>
     </div>
   );
