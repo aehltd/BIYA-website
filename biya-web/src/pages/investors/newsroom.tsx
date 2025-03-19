9b826bde7bb9541e463e34033199e8a076aa8765
Junyu (Tim) Yao <197yjy@gmail.com>
2025-03-19 13:43:11 -0400

    Update newsroom.tsx
    remove coming soon

--- biya-web/src/pages/investors/newsroom.tsx
+++ biya-web/src/pages/investors/newsroom.tsx
@@ -2,7 +2,7 @@ import IrPageBanner from "../../components/banner/irPageBanner";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 
-import ComingSoon from "@/components/upComing";
+// import ComingSoon from "@/components/upComing";
 import QModFooter from "@/components/QModFooter";
 import QMod from "@/components/QMod";
 
@@ -40,7 +40,7 @@ export default function Newsroom() {
           Press Releases
         </Typography>
         <Divider />
-        <ComingSoon/>
+        {/* <ComingSoon/> */}
         <div>
           <div>
             <QMod
