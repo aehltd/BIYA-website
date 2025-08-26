6e461806771e53d9828670c2a586aa16716ae08f
Ryan Jiang <jiangr0421@gmail.com>
2025-08-26 11:48:43 -0400

    feat: add biya media page

---
+++ biya-web/src/components/banner/biyaMediaBanner.tsx
@@ -0,0 +1,11 @@
+export default function BiyaMediaBanner() {
+  return (
+    <div className="relative h-[65vh] w-full flex items-center justify-center overflow-hidden">
+      <img
+        src="https://res.cloudinary.com/aehl/image/upload/v1756222003/BIYA-Media_pl8byr.png"
+        alt="Biya Media Banner"
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+    </div>
+  );
+}
